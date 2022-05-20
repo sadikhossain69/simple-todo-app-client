ff365874d57c942b8f0c615cb8bf2949e3c8d362
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-21 04:37:33 +0600

    task required information collected

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Home = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+
+    const handlTaskSubmit = data => {
+        console.log(data);
+        reset()
+    }
+
     return (
         <div>
             <div className='text-center mt-10 space-y-3'>
@@ -11,11 +20,40 @@ const Home = () => {
                     Enter What Work You Have To Do😊
                 </h2>
             </div>
-            <form className='text-center space-y-3 mt-5'>
-                <input className='input input-bordered w-full md:w-2/5 lg:w-2/5 font-bold' type="text" placeholder='Your Name' /><br />
-                <input className='input input-bordered w-full md:w-2/5 lg:w-2/5 font-bold' type="text" placeholder='Your Task' /><br />
-                <input className='text-center bg-blue-600 text-white font-semibold py-1 px-4 rounded hover:bg-blue-500 hover:duration-300 hover:scale-125 cursor-pointer' type="submit" value="Submit" />
-            </form>
+            <div className=''>
+                <form onSubmit={handleSubmit(handlTaskSubmit)} className='text-center space-y-3 mt-5'>
+                    <input {...register('name', {
+                        required: {
+                            value: true,
+                            message: 'Your Name is Required'
+                        },
+                        minLength: {
+                            value: 3,
+                            message: "Your Name Will Must 3 Character or Longer"
+                        }
+                    })} className='input input-bordered w-full md:w-2/5 lg:w-2/5 font-bold' type="text" placeholder='Your Name' /><br />
+                    <div>
+                        {errors.name?.type === 'required' && <p className="label-text-alt text-red-500 font-semibold">{errors.name.message}</p>}
+                        {errors.name?.type === 'minLength' && <p className="label-text-alt text-red-500 font-semibold">{errors.name.message}</p>}
+                    </div>
+                    <input {...register('task', {
+                        required: {
+                            value: true,
+                            message: 'Your Task is Required'
+                        },
+                        minLength: {
+                            value: 3,
+                            message: 'Your Task Will Must 3 Character or Longer'
+                        }
+                    })} className='input input-bordered w-full md:w-2/5 lg:w-2/5 font-bold' type="text" placeholder='Your Task' /><br />
+                    <div>
+                        {errors.task?.type === 'required' && <p className="label-text-alt text-red-500 font-semibold">{errors.task.message}</p>}
+                        {errors.task?.type === 'minLength' && <p className="label-text-alt text-red-500 font-semibold">{errors.task.message}</p>}
+                    </div>
+
+                    <input className='text-center bg-blue-600 text-white font-semibold py-1 px-4 rounded hover:bg-blue-500 hover:duration-300 hover:scale-125 cursor-pointer' type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 };
